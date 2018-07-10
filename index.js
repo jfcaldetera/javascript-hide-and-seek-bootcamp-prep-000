03b187b5721565db305f080293dfaf1eebc28c0d
Jhenna Foronda <jfcaldetera@gmail.com>
2018-07-10 16:04:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ function nestedTarget()
 {
   return document.querySelector('#nested.target')
 }
+
+function increaseRankBy(n)
+{
+  const list = document.getElementById('app').querySelector('ul.ranked-list li')
+
+  for (let i = 0; i < lis.length; i++)
+  {
+    lis[i].innerHTML(i + n).toString()
+  }
+}
